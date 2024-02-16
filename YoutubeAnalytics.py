949662b2b91f94e4e2162e59c72627132c21a7c8
Dung Trung <tdungdg.1901@gmail.com>
2024-02-16 17:52:50 +0700

    connect to kafka and stream to telegram

--- YoutubeAnalytics.py
+++ YoutubeAnalytics.py
@@ -1,32 +1,64 @@
 import json
+import logging
+
 import requests
-from constant import YOUTUBE_API_KEY
-from pprint import pprint
 from kafka import KafkaProducer
 
+from constant import YOUTUBE_API_KEY, PLAYLIST_ID
+
+
+def fetch_page(url, parameters, page_token=None):
+    params = {**parameters, "key": YOUTUBE_API_KEY, "page_token": page_token}
+    response = requests.get(url, params)
+    payload = json.loads(response.text)
+
+    return payload
+
+
+def fetch_page_lists(url, parameters, page_token=None):
+    while True:
+        payload = fetch_page(url, parameters, page_token)
+        yield from payload["items"]
+
+        page_token = payload.get("nextPageToken")
+        if page_token is None:
+            break
+
+
+def format_response(video):
+    video_res = {
+        "title": video["snippet"]["title"],
+        "likes": int(video["statistics"].get("likeCount", 0)),
+        "comments": int(video["statistics"].get("commentCount", 0)),
+        "views": int(video["statistics"].get("viewCount", 0)),
+        "favorites": int(video["statistics"].get("favoriteCount", 0)),
+        "thumbnail": video["snippet"]["thumbnails"]["default"]["url"],
+    }
+    return video_res
+
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
-    response = requests.get(
-        "https://www.googleapis.com/youtube/v3/videos",
-        params={
-            "key": YOUTUBE_API_KEY,
-            "id": "HwgrFT2o4LQ",
-            "part": "snippet, statistics, status",
-        },
-    )
-    # print(response.text)
-    response = json.loads(response.text)["items"]
-    for video in response:
-        video_res = {
-            "title": video["snippet"]["title"],
-            "likes": int(video["statistics"].get("likeCount", 0)),
-            "comments": int(video["statistics"].get("commentCount", 0)),
-            "views": int(video["statistics"].get("viewsCount", 0)),
-            "favorites": int(video["statistics"].get("favoriteCount", 0)),
-            "thumbnail": video["snippet"]["thumbnails"]["default"]["url"],
-        }
 
-        pprint(video_res)
-        producer.send("youube_videos", json.dumps(video_res).encode("utf-8"))
+    for video_item in fetch_page_lists(
+        "https://www.googleapis.com/youtube/v3/playlistItems",
+        {"playlistId": PLAYLIST_ID, "part": "snippet,contentDetails"},
+        None,
+    ):
+        video_id = video_item["contentDetails"]["videoId"]
 
-        producer.flush()
+        for video in fetch_page_lists(
+            "https://www.googleapis.com/youtube/v3/videos",
+            {"id": video_id, "part": "snippet,statistics"},
+            None,
+        ):
+            # logging.info("Video here => %s", pprint(format_response(video)))
+            producer.send(
+                "youtube_videos",
+                json.dumps(format_response(video)).encode("utf-8"),
+                key=video_id.encode("utf-8"),
+            )
+            print("Sent ", video["snippet"]["title"])
+            # producer.flush()
--- constant.py
+++ constant.py
@@ -5,6 +5,6 @@ import os
 parser = configparser.ConfigParser()
 parser.read(os.path.join(os.path.dirname(__file__), "config/config.local"))
 
-
+PLAYLIST_ID = parser.get("youtube", "PLAYLIST_ID")
 YOUTUBE_API_KEY = parser.get("youtube", "API_KEY")
 # print(YOUTUBE_API_KEY)
